26de9e8eba39ee069b2f371b12b5db356df92aa6
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.com>
2020-01-01 18:55:05 +0200

    Exercise 27

---
+++ src/Week2/Exercise27.java
@@ -0,0 +1,13 @@
+package Week2;
+
+public class Exercise27 {
+    public static void main(String[] args) {
+        //Exercise 27: From one to a hundred
+
+        int number = 1;
+        while (number <= 100) {
+            System.out.println(number);
+             number++;
+        }
+    }
+}

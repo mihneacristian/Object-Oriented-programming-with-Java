4fb1308b4daeb874e2cbcb44b54d57e3c56c511b
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.com>
2020-01-02 20:13:44 +0200

    Exercise 37

---
+++ src/Week2/Exercise37.java
@@ -0,0 +1,13 @@
+package Week2;
+
+public class Exercise37 {
+    public static void main(String[] args) {
+        //Exercise 37: Print out text
+
+        printText();
+    }
+
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+}

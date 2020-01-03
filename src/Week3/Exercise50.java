27a4fd9ec7187e74c3050ff01c98ca381e204624
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.com>
2020-01-03 17:30:32 +0200

    Exercise 50

---
+++ src/Week3/Exercise50.java
@@ -0,0 +1,32 @@
+package Week3;
+
+import java.util.Scanner;
+
+public class Exercise50 {
+    public static char separatingCharacters(String text, int number) {
+        return text.charAt(number - 1);
+    }
+
+    public static void printCharacters(char firstCharacter, char secondCharacter, char thirdCharacter) {
+        System.out.println("1. character: " + firstCharacter);
+        System.out.println("2. character: " + secondCharacter);
+        System.out.println("3. character: " + thirdCharacter);
+    }
+
+    public static void main(String[] args) {
+        //Exercise 50: Separating first characters
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Type your name: ");
+        String userName = in.nextLine();
+
+        if (userName.length() < 3) {
+            System.out.println("");
+        } else {
+            char firstChar = separatingCharacters(userName, 1);
+            char secondChar = separatingCharacters(userName, 2);
+            char thirdChar = separatingCharacters(userName, 3);
+            printCharacters(firstChar, secondChar, thirdChar);
+        }
+    }
+}

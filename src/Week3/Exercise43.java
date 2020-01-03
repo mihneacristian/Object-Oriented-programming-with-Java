05a6b780b10c42ef7f4878495546c10d03f01c8d
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.com>
2020-01-03 13:16:08 +0200

    Exercise 43

---
+++ src/Week3/Exercise43.java
@@ -0,0 +1,16 @@
+package Week3;
+
+public class Exercise43 {
+    public static int sum(int number1, int number2, int number3, int number4) {
+        // write program code here
+        // remember that the method needs a return in the end
+        return number1 + number2 + number3 + number4;
+    }
+
+    public static void main(String[] args) {
+        //Exercise 43: Sum of numbers
+
+        int answer = sum(4,3,6,1);
+        System.out.println("Sum: " + answer);
+    }
+}

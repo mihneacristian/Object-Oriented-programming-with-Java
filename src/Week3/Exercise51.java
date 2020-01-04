43fc2cbaf23fcdece812a741e281abda5c69ca18
Popa Mihnea Cristian <mihnea-cristian.popa@outlook.com>
2020-01-04 19:40:22 +0200

    Exercise 51

--- src/Week3/Exercise51.java
+++ src/Week3/Exercise51.java
@@ -3,27 +3,23 @@ package Week3;
 import java.util.Scanner;
 
 public class Exercise51 {
-    public static char separatingCharacters(String text, int number) {
-        return text.charAt(number);
-    }
-
-    public static char numberOfCharacters (String text) {
-        return (char) (text.length() - 1);
-    }
-
-    public static void printCharacters(String text, int numberOfLetters) {
+    public static String separateCharacters(String userName) {
         int counter = 0;
-        while (numberOfLetters >= counter) {
-            char characters = separatingCharacters(text, numberOfLetters);
-            System.out.println((counter + 1) + ". character: " + characters);
+        while (counter < userName.length()) {
+            System.out.println(counter + 1 + ". character: " + userName.charAt(counter));
             counter++;
         }
-
+        return "";
     }
+
     public static void main(String[] args) {
         //Exercise 51: Separating characters
 
         Scanner in = new Scanner(System.in);
-        System.out.println();
+        System.out.println("Type your name: ");
+        String userName = in.nextLine();
+
+        String answer = separateCharacters(userName);
+        System.out.println(answer);
     }
 }
